87218e3af41194bff749fca4ea18e9e43ecbfb76
amitsharma7565 <amitsharma7565@gmail.com>
2024-03-19 12:44:37 +0530

    checking the server

--- src/main/java/com/example/demo/JenkinsPracticeApplication.java
+++ src/main/java/com/example/demo/JenkinsPracticeApplication.java
@@ -8,7 +8,7 @@ public class JenkinsPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPracticeApplication.class, args);
-		System.out.println("hi welocome your application is deployed ");
+		System.out.println("hi welocome your application is deployed");
 	}
 
 }
