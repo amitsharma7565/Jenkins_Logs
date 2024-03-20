8bfd09725fa5575214c6bcc2cd49efc441f66a90
amitsharma7565 <amitsharma7565@gmail.com>
2024-03-20 13:41:08 +0530

    second time

--- src/main/java/com/example/demo/JenkinsPracticeApplication.java
+++ src/main/java/com/example/demo/JenkinsPracticeApplication.java
@@ -6,6 +6,7 @@ public class JenkinsPracticeApplication {
 	public static void main(String[] args) {
 	
 		System.out.println("hi welocome your application is deployed ");
+		System.out.println("hi again deploy in the server");
 	}
 
 }
