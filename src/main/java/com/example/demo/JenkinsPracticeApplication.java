0eb949621ea7c5ec88bd46690ece8ee89928e584
amitsharma7565 <amitsharma7565@gmail.com>
2024-03-20 12:53:41 +0530

    remove annotation

--- src/main/java/com/example/demo/JenkinsPracticeApplication.java
+++ src/main/java/com/example/demo/JenkinsPracticeApplication.java
@@ -1,13 +1,10 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class JenkinsPracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JenkinsPracticeApplication.class, args);
+	
 		System.out.println("hi welocome your application is deployed ");
 	}
 
